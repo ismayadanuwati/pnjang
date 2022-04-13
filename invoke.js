59e00c1be27f503b11f669a74d4b0864da46ce7d
ismayadanuwati <103593869+ismayadanuwati@users.noreply.github.com>
2022-04-14 00:00:35 +0700

    Create invoke.js

---
+++ invoke.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '9eca90e031749c3ab1f554aa756170ca',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplayformats.com/9eca90e031749c3ab1f554aa756170ca/invoke.js"></scr' + 'ipt>');
